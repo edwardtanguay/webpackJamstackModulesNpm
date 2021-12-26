c45c524871165ff705e4e103add17c9e521734eb
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-12-26 19:17:49 +0100

    only axios

--- src/components/SectionUsers.js
+++ src/components/SectionUsers.js
@@ -3,19 +3,6 @@ import axios from 'axios';
 
 const apiUrl = 'https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/json/employees.json';
 
-// (async () => {
-// 	const response = await fetch(apiUrl);
-// 	if (response.ok) {
-// 		const users = await response.json();
-// 		users.forEach(user => {
-// 			// console.log(user.firstName);
-// 		})
-// 	} else {
-// 		console.log(response);
-// 	}
-// })();
-
-
 (async () => {
 	const response = await axios.get(apiUrl).catch(err => {
 		console.log(`Error fetching users: ${err}`);
