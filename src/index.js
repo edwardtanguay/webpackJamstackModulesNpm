a558ef95508c44cd56aa75889b337cdf92de1300
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-12-26 18:08:43 +0100

    normalize sections

--- src/index.js
+++ src/index.js
@@ -11,19 +11,22 @@ document.querySelector('title').innerHTML = 'Basic Webpack Site';
 
 document.querySelector('.app').innerHTML = `
 
-<div class="exampleSection">
-${DisplayHeader({ supertitle: 'Example of reading a JSON file', header: 'Users'})}
+<div class="section">
+	${DisplayHeader({ supertitle: 'Example of reading a JSON file', header: 'Users' })}
 	<ul class="users">
 	${users.map(user => '<li>' + user.lastName + ', ' + user.firstName + '</li>').join('')}
 	</ul>
 </div>
 
-${DisplayHeader({ supertitle: 'Example of custom components and npm package', header: 'Web Technology'})}
-	${SectionWebTechnology({name: "Webpack", created: 2012, included: true})}
-	${SectionWebTechnology({name: "Browserify", created: 2010, included: false})}
+<div class="section">
+	${DisplayHeader({ supertitle: 'Example of custom components and npm package', header: 'Web Technology' })}
+	${SectionWebTechnology({ name: "Webpack", created: 2012, included: true })}
+	${SectionWebTechnology({ name: "Browserify", created: 2010, included: false })}
 </div>
 
-${DisplayHeader({supertitle: 'Example of npm package and interactivity', header: 'Word Search'})}
-${SectionWordSearch()}
+<div class="section">
+	${DisplayHeader({ supertitle: 'Example of npm package and interactivity', header: 'Word Search' })}
+	${SectionWordSearch()}
+</div>
 `;
 
